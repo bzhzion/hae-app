18570f208157929f9981d923166bdcbd641d6e91
KaO <thierryoliv@gmail.com>
2026-06-15 01:06:50 +0200

    test: logo seul sur fond rouge

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Image } from 'react-native';
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#A00000' }}>
-      <Text style={{ color: '#fff', fontSize: 48, fontWeight: '700' }}>Bonjour</Text>
+      <Image source={require('../assets/icon-transparent.png')} style={{ width: 120, height: 120 }} resizeMode="contain" />
     </View>
   );
 }
